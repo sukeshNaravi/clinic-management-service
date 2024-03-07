c9b2fd6e87619bebcf8bfc219ee31f8eac982f54
Sukesh Naravi <sukesh.pn@rtwohealthcare.com>
2024-03-07 14:48:52 +0530

    Initial commit

---
+++ src/main/java/com/shadow/clinicmanagementservice/ClinicManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadow.clinicmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadow/clinicmanagementservice/ClinicManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.clinicmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
